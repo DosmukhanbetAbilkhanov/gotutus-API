867c216604006231ebc9e6a4de1be6af08efef1d
Dosmukhanbet <dosmukhanbet@gmail.com>
2026-03-27 12:22:04 +0500

    seeding commented

--- routes/web.php
+++ routes/web.php
@@ -12,48 +12,48 @@ Route::get('/', function () {
 Route::get('/create-admin', function () {
 
 
-        $now = now();
+        // $now = now();
 
-        $activityTypes = [
-            ['slug' => 'beer', 'icon' => "\u{1F37A}", 'ru' => 'Пиво', 'kz' => 'Сыра', 'en' => 'Beer'],
-            ['slug' => 'coffee', 'icon' => "\u{2615}", 'ru' => 'Кофе', 'kz' => 'Кофе', 'en' => 'Coffee'],
-            ['slug' => 'sushi', 'icon' => "\u{2615}", 'ru' => ' Суши', 'kz' => ' Суши', 'en' => 'Sushi'],
-            ['slug' => 'fast_food', 'icon' => "\u{2615}", 'ru' => 'Фаст-Фуд', 'kz' => 'Фаст-Фуд', 'en' => 'Fast Food'],
-            ['slug' => 'kumys', 'icon' => "\u{2615}", 'ru' => 'Кумыс', 'kz' => 'Кумыс', 'en' => 'Kumys'],
-            ['slug' => 'bathhouse', 'icon' => "\u{1F9D6}", 'ru' => 'Баня', 'kz' => 'Монша', 'en' => 'Bathhouse'],
-            ['slug' => 'walk', 'icon' => "\u{1F6B6}", 'ru' => 'Прогулка', 'kz' => 'Серуен', 'en' => 'Walk'],
-            ['slug' => 'concert', 'icon' => "\u{1F6B6}", 'ru' => 'Концерт', 'kz' => 'Концерт', 'en' => 'Concert'],
-            ['slug' => 'bowling', 'icon' => "\u{1F3B3}", 'ru' => 'Боулинг', 'kz' => 'Боулинг', 'en' => 'Bowling'],
-            ['slug' => 'billiards', 'icon' => "\u{1F3B1}", 'ru' => 'Бильярд', 'kz' => 'Бильярд', 'en' => 'Billiards'],
-            ['slug' => 'hookah', 'icon' => "\u{1F4A8}", 'ru' => 'Кальян', 'kz' => 'Кальян', 'en' => 'Hookah'],
-            ['slug' => 'karaoke', 'icon' => "\u{1F3A4}", 'ru' => 'Караоке', 'kz' => 'Караоке', 'en' => 'Karaoke'],
-            ['slug' => 'restaurant', 'icon' => "\u{1F37D}\u{FE0F}", 'ru' => 'Ресторан', 'kz' => 'Мейрамхана', 'en' => 'Restaurant'],
-            ['slug' => 'kvest', 'icon' => "\u{26BD}", 'ru' => 'Квест', 'kz' => 'Квест', 'en' => 'Kvest'],
-            ['slug' => 'paintball', 'icon' => "\u{26BD}", 'ru' => 'Пейнтбол', 'kz' => 'Пейнтбол', 'en' => 'Paintball'],
-            ['slug' => 'pc_club', 'icon' => "\u{26BD}", 'ru' => 'Компьютерный Клуб', 'kz' => 'Компьютерный Клуб', 'en' => 'PC Club'],
-            ['slug' => 'quiz', 'icon' => "\u{26BD}", 'ru' => 'Квиз', 'kz' => 'Квиз', 'en' => 'Quiz'],
-            ['slug' => 'tennis', 'icon' => "\u{26BD}", 'ru' => 'Теннис', 'kz' => 'Теннис', 'en' => 'Tennis'],
-            ['slug' => 'football', 'icon' => "\u{26BD}", 'ru' => 'Футбол', 'kz' => 'Футбол', 'en' => 'Football'],
-            ['slug' => 'cinema', 'icon' => "\u{1F3AC}", 'ru' => 'Кино', 'kz' => 'Кино', 'en' => 'Cinema'],
-            ['slug' => 'board-games', 'icon' => "\u{1F3B2}", 'ru' => 'Настолки', 'kz' => 'Үстел ойындары', 'en' => 'Board Games'],
-        ];
+        // $activityTypes = [
+        //     ['slug' => 'beer', 'icon' => "\u{1F37A}", 'ru' => 'Пиво', 'kz' => 'Сыра', 'en' => 'Beer'],
+        //     ['slug' => 'coffee', 'icon' => "\u{2615}", 'ru' => 'Кофе', 'kz' => 'Кофе', 'en' => 'Coffee'],
+        //     ['slug' => 'sushi', 'icon' => "\u{2615}", 'ru' => ' Суши', 'kz' => ' Суши', 'en' => 'Sushi'],
+        //     ['slug' => 'fast_food', 'icon' => "\u{2615}", 'ru' => 'Фаст-Фуд', 'kz' => 'Фаст-Фуд', 'en' => 'Fast Food'],
+        //     ['slug' => 'kumys', 'icon' => "\u{2615}", 'ru' => 'Кумыс', 'kz' => 'Кумыс', 'en' => 'Kumys'],
+        //     ['slug' => 'bathhouse', 'icon' => "\u{1F9D6}", 'ru' => 'Баня', 'kz' => 'Монша', 'en' => 'Bathhouse'],
+        //     ['slug' => 'walk', 'icon' => "\u{1F6B6}", 'ru' => 'Прогулка', 'kz' => 'Серуен', 'en' => 'Walk'],
+        //     ['slug' => 'concert', 'icon' => "\u{1F6B6}", 'ru' => 'Концерт', 'kz' => 'Концерт', 'en' => 'Concert'],
+        //     ['slug' => 'bowling', 'icon' => "\u{1F3B3}", 'ru' => 'Боулинг', 'kz' => 'Боулинг', 'en' => 'Bowling'],
+        //     ['slug' => 'billiards', 'icon' => "\u{1F3B1}", 'ru' => 'Бильярд', 'kz' => 'Бильярд', 'en' => 'Billiards'],
+        //     ['slug' => 'hookah', 'icon' => "\u{1F4A8}", 'ru' => 'Кальян', 'kz' => 'Кальян', 'en' => 'Hookah'],
+        //     ['slug' => 'karaoke', 'icon' => "\u{1F3A4}", 'ru' => 'Караоке', 'kz' => 'Караоке', 'en' => 'Karaoke'],
+        //     ['slug' => 'restaurant', 'icon' => "\u{1F37D}\u{FE0F}", 'ru' => 'Ресторан', 'kz' => 'Мейрамхана', 'en' => 'Restaurant'],
+        //     ['slug' => 'kvest', 'icon' => "\u{26BD}", 'ru' => 'Квест', 'kz' => 'Квест', 'en' => 'Kvest'],
+        //     ['slug' => 'paintball', 'icon' => "\u{26BD}", 'ru' => 'Пейнтбол', 'kz' => 'Пейнтбол', 'en' => 'Paintball'],
+        //     ['slug' => 'pc_club', 'icon' => "\u{26BD}", 'ru' => 'Компьютерный Клуб', 'kz' => 'Компьютерный Клуб', 'en' => 'PC Club'],
+        //     ['slug' => 'quiz', 'icon' => "\u{26BD}", 'ru' => 'Квиз', 'kz' => 'Квиз', 'en' => 'Quiz'],
+        //     ['slug' => 'tennis', 'icon' => "\u{26BD}", 'ru' => 'Теннис', 'kz' => 'Теннис', 'en' => 'Tennis'],
+        //     ['slug' => 'football', 'icon' => "\u{26BD}", 'ru' => 'Футбол', 'kz' => 'Футбол', 'en' => 'Football'],
+        //     ['slug' => 'cinema', 'icon' => "\u{1F3AC}", 'ru' => 'Кино', 'kz' => 'Кино', 'en' => 'Cinema'],
+        //     ['slug' => 'board-games', 'icon' => "\u{1F3B2}", 'ru' => 'Настолки', 'kz' => 'Үстел ойындары', 'en' => 'Board Games'],
+        // ];
 
-        foreach ($activityTypes as $type) {
-            $typeId = DB::table('activity_types')->insertGetId([
-                'slug' => $type['slug'],
-                'bg_photo' => null,
-                'icon' => $type['icon'],
-                'is_active' => true,
-                'created_at' => $now,
-                'updated_at' => $now,
-            ]);
+        // foreach ($activityTypes as $type) {
+        //     $typeId = DB::table('activity_types')->insertGetId([
+        //         'slug' => $type['slug'],
+        //         'bg_photo' => null,
+        //         'icon' => $type['icon'],
+        //         'is_active' => true,
+        //         'created_at' => $now,
+        //         'updated_at' => $now,
+        //     ]);
 
-            DB::table('activity_type_translations')->insert([
-                ['activity_type_id' => $typeId, 'language_code' => 'ru', 'name' => $type['ru']],
-                ['activity_type_id' => $typeId, 'language_code' => 'kz', 'name' => $type['kz']],
-                ['activity_type_id' => $typeId, 'language_code' => 'en', 'name' => $type['en']],
-            ]);
-        }
+        //     DB::table('activity_type_translations')->insert([
+        //         ['activity_type_id' => $typeId, 'language_code' => 'ru', 'name' => $type['ru']],
+        //         ['activity_type_id' => $typeId, 'language_code' => 'kz', 'name' => $type['kz']],
+        //         ['activity_type_id' => $typeId, 'language_code' => 'en', 'name' => $type['en']],
+        //     ]);
+        // }
 
         // $cities = [
         //     ['en' => 'Aktobe', 'ru' => 'Актобе', 'kz' => 'Ақтөбе'],
