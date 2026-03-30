e0b7588045e458fa9b45a3354be2a1b3464e5698
Dosmukhanbet <dosmukhanbet@gmail.com>
2026-03-30 23:19:02 +0500

    TestRedisQueue job created

---
+++ app/Jobs/TestRedisQueue.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class TestRedisQueue implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        logger('Redis queue is working!');
+    }
+}
